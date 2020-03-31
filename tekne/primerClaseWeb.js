7f3f8d4562c1531d974f363b300bc95962fba100
Axel <axdiaz@users.noreply.github.com>
2020-03-31 16:06:22 -0300

    Add files via upload

---
+++ tekne/primerClaseWeb.js
@@ -0,0 +1,13 @@
+// esto es un comentario. un comentairo es cuando escribo texto en un archivo de codigo 
+// y que no quiero que la compu lo lea como si fuera codigo.
+
+
+//la palabra clave var se utiliza para "crear" una variable en memoria.
+var nombre = "Lola"  //cadena de caracteres / String
+var apellido = "Heer"
+var edad = 23
+
+
+//concatenacion
+console.log("Mi nombre es " + nombre + " mi apellido es " + apellido + " y mi edad es: " + edad)
+
